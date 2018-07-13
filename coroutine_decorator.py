a7175394f477d9d66f92dd1778cc318565ba9827
zachang <zachangdawuda@gmail.com>
2018-07-13 17:06:52 +0100

    implemented coroutine to with decorator

---
+++ coroutine_decorator.py
@@ -0,0 +1,20 @@
+def coroutine_decorator(func):
+    def start(*args,**kwargs):
+        cr = func(*args,**kwargs)
+        next(cr)
+        return cr
+    return start
+
+@coroutine_decorator
+def first_coroutine(pattern):
+    print(f"Looking for ... {pattern}")
+    while True:
+        line = (yield) # yield expression
+        if pattern in line:
+            print(line)
+
+_coroutine = first_coroutine("Django")
+_coroutine.send("Yeah, but no, but yeah, but no")
+_coroutine.send("A series on Django's models")
+_coroutine.send("A series of tubes")
+_coroutine.send("Django rocks!")
